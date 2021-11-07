57f7d580ec3b8b6121ee16e996dc8600b9d4c6c6
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-11-07 12:22:22 +0200

    Create project

---
+++ src/main/java/com/example/tadoblog/TadoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.tadoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadoBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tadoblog/TadoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tadoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadoBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
