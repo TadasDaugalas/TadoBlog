71ee2a314142548bc38a946b604e8bef7147c882
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-11-14 16:02:36 +0200

    Add first db-schema

---
+++ src/main/resources/db/db-schema.sql
@@ -0,0 +1,8 @@
+create table Cards(
+                         id varchar(36) not null primary key,
+                         cardTitle varchar(200) not null,
+                         cardText varchar (50) not null,
+                         cardComments varchar (150),
+                         cardCommentsCounter integer,
+                         cardRepilies varchar (150),
+);
