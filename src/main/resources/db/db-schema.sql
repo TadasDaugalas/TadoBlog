ab303386a2f1c1c517903013bee75ea82f116261
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-11-14 20:01:27 +0200

    update schema

--- src/main/resources/db/db-schema.sql
+++ src/main/resources/db/db-schema.sql
@@ -1,8 +1,8 @@
-create table Cards(
+create table CARDS(
                          id varchar(36) not null primary key,
                          cardTitle varchar(200) not null,
                          cardText varchar (50) not null,
                          cardComments varchar (150),
                          cardCommentsCounter integer,
-                         cardRepilies varchar (150),
-);
+                         cardRepilies varchar (150);
+)
