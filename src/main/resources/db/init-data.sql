cde68c5b82ab6a8ad7d8f8addb25e6f700460751
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-11-14 20:01:14 +0200

    add some data

---
+++ src/main/resources/db/init-data.sql
@@ -0,0 +1,6 @@
+insert into CARDS(id,card_title,card_text,card_comments_counter)
+values
+('48a95af7-8b83-4a08-8001-0f865db8ea26','pirmas','Labad iena',0),
+('38a95af7-8b83-4a08-8001-0f86saxgfa26','antras','Viso gero',0),
+('28a95af7-8b83-4a08-8001-0f86vcb8ea26','trecias','Viso gero',0),
+('18a95af7-8b83-4a08-8001-0f865dbhgf26','ketvirtas','Viso gero',0);
