ea6fff6ab6f9ff14d0a7c478458d0a4feebf710f
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-11-21 12:34:47 +0200

    Add fragments,updated with init data

---
+++ src/main/java/com/example/tadoblog/advice/CardControlerAdvice.java
@@ -0,0 +1,33 @@
+package com.example.tadoblog.advice;
+import com.example.tadoblog.exeption.CardNotExistExeption;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.beans.PropertyEditor;
+import java.util.Date;
+
+
+@ControllerAdvice
+public class CardControlerAdvice {
+
+    @ExceptionHandler(CardNotExistExeption.class)
+    public String hanglingProductNotExist(CardNotExistExeption cardNotExistExeption, Model model){
+        model.addAttribute("cardId" , cardNotExistExeption.getCardId());
+
+        return "cardNotFound";
+    }
+    @InitBinder
+    public void initStringBinder(WebDataBinder webDataBinder){
+        PropertyEditor editor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class,editor);
+    }
+    @ModelAttribute("currentDate")
+    public Date currentDate(){
+        return new Date();
+    }
+}
--- src/main/resources/db/init-data.sql
+++ src/main/resources/db/init-data.sql
@@ -1,6 +1,6 @@
 insert into CARDS(id,card_title,card_text,card_comments_counter)
 values
-('48a95af7-8b83-4a08-8001-0f865db8ea26','pirmas','Labad iena',0),
-('38a95af7-8b83-4a08-8001-0f86saxgfa26','antras','Viso gero',0),
-('28a95af7-8b83-4a08-8001-0f86vcb8ea26','trecias','Viso gero',0),
-('18a95af7-8b83-4a08-8001-0f865dbhgf26','ketvirtas','Viso gero',0);
+('056b808d-97c3-4a06-b588-1af008374145','pirmas','Labad iena',0),
+('b059043f-14b5-4ecc-ad97-679f74359cf1','antras','Viso gero',0),
+('b9a0e092-b98f-4159-9a9e-7ffaf1270392','trecias','Viso gero',0),
+('992576b8-a103-496f-9b0b-b0483c407570','ketvirtas','Viso gero',0);
